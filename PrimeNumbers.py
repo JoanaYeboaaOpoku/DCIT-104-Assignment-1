395b9cb9d8173999c603a41a109bfaefea171381
JoanaYeboaaOpoku <103155781+JoanaYeboaaOpoku@users.noreply.github.com>
2022-08-07 15:02:47 +0000

    Initial commit

---
+++ PrimeNumbers.py
@@ -0,0 +1,62 @@
+# number = int(input("Enter your number: "))
+# primeNum = []
+
+# def isPrime(y):
+#     i = 2
+#     while(i < number):  
+#         if number % i == 0: 
+#            return False
+#         i = i + 1
+#     return True
+
+# def printNum(x):
+#     k = 2  
+#     while k <= x:
+#         if isPrime(k):
+#             primeNum.append(k)
+#         k = k + 1        
+     
+# printNum(number)
+
+# numOfPrime = len(primeNum)
+# sum = 0
+
+# j= 0
+# while j < numOfPrime:
+#     sum += primeNum[j]
+# j = j + 1  
+
+# print(sum)
+
+primeNum = []
+
+number=int(input("Enter Number: "))
+def isPrime(y):
+    i = 2
+    while (i < y):
+        if y % i == 0:
+            return False
+        i = i + 1
+    return True
+
+
+def printNum(x):
+    k = 2
+    while k <= x:
+        if isPrime(k):
+            primeNum.append(k)
+        k = k + 1
+
+
+printNum(number)
+
+numOfPrime = len(primeNum)
+sum = 0
+
+j = 0
+while j < numOfPrime:
+    sum += primeNum[j]
+    j = j + 1
+
+print(sum)
+print("end")
